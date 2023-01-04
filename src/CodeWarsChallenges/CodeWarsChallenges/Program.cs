57728d3dcff6469fe89eddc6709da13773bb10e4
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-01-04 16:18:32 -0300

    desafio 'L1: Set Alarm' - RESOLVIDO

--- src/CodeWarsChallenges/CodeWarsChallenges/Program.cs
+++ src/CodeWarsChallenges/CodeWarsChallenges/Program.cs
@@ -1,23 +1,21 @@
-﻿//CODE WARS CHALLENGE - 'Opposites Attract' - RESOLVIDO
-
-//Timmy e Sarah acham que estão apaixonados, mas perto de onde moram, eles só saberão quando colherem uma flor cada um.
-//Se uma das flores tiver um número par de pétalas e a outra tiver um número ímpar de pétalas, significa que eles estão apaixonados.
-//Escreva uma função que pegue o número de pétalas de cada flor e retorne true se elas estiverem apaixonadas e false se não estiverem.
+﻿//CODE WARS CHALLENGE - 'L1: Set Alarm' - RESOLVIDO
 
 /*
- * [Test]
- * Assert.AreEqual(true, LoveDetector.lovefunc(1, 4));
- * Assert.AreEqual(false, LoveDetector.lovefunc(2, 2));
- * Assert.AreEqual(true, LoveDetector.lovefunc(0, 1));
- * Assert.AreEqual(false, LoveDetector.lovefunc(0, 0));
+Escreva uma função chamada setAlarm que recebe dois parâmetros. 
+O primeiro parâmetro, Employees , é verdadeiro sempre que você está empregado e o segundo parâmetro, vacation , 
+é verdadeiro sempre que você está de férias.
+A função deve retornar true se você estiver empregado e não estiver de férias 
+(porque essas são as circunstâncias nas quais você precisa definir um alarme). 
+Caso contrário, deve retornar falso.
+
+[Test]
+Assert.AreEqual(false, Kata.SetAlarm(true, true));
+Assert.AreEqual(false, Kata.SetAlarm(false, true));
+Assert.AreEqual(true, Kata.SetAlarm(true, false));
+Assert.AreEqual(false, Kata.SetAlarm(false, false));
  */
 
-static bool LoveFunc(int flor1, int flor2)
-{
-    if ((flor1 % 2 == 0 && flor2 % 2 != 0) || (flor2 % 2 == 0 && flor1 % 2 != 0))
-        return true;
 
-    return false;
-}
-
+static bool SetAlarm(bool employed, bool vacation)
+    => employed && !vacation;
 
