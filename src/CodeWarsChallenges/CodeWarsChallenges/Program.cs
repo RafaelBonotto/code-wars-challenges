2746e0bce4160dfe8bebc983f435c0630b973f0f
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-01-05 10:53:15 -0300

    desafio 'Who likes it?' - RESOLVIDO

--- src/CodeWarsChallenges/CodeWarsChallenges/Program.cs
+++ src/CodeWarsChallenges/CodeWarsChallenges/Program.cs
@@ -1,4 +1,4 @@
-﻿//CODE WARS CHALLENGE - 'Who likes it?'
+﻿//CODE WARS CHALLENGE - 'Who likes it?' - RESOLVIDO
 /*
 Você provavelmente conhece o sistema de "curtir" do Facebook e outras páginas. 
 As pessoas podem "curtir" postagens de blog, fotos ou outros itens. Queremos criar o texto que deve ser exibido ao lado de tal item.
@@ -14,5 +14,28 @@ Ele deve retornar o texto de exibição conforme os exemplos:
 
 static string Likes(string[] name)
 {
-    throw new NotImplementedException();
+    int len = name.Length;
+    string response = string.Empty;
+
+    switch (len)
+    {
+        case 0:
+             response = "no one likes this";
+            break;
+        case 1:
+             response = $"{name[0]} likes this";
+            break;
+        case 2:
+             response = $"{name[0]} and {name[1]} like this";
+            break;
+        case 3:
+             response = $"{name[0]}, {name[1]} and {name[2]} like this";
+            break;
+        case > 3:
+             response = $"{name[0]}, {name[1]} and {len - 2} others like this";
+            break;
+        default:
+            break;
+    }
+    return response;
 }
