4bd5dcee05efcc1e0a1afe50169e0c010842c1f9
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-01-05 15:47:14 -0300

    desafio 'Detect Pangram'

--- src/CodeWarsChallenges/CodeWarsChallenges/Program.cs
+++ src/CodeWarsChallenges/CodeWarsChallenges/Program.cs
@@ -1,41 +1,15 @@
-﻿//CODE WARS CHALLENGE - 'Who likes it?' - RESOLVIDO
+﻿//CODE WARS CHALLENGE - 'Detect Pangram' 
 /*
-Você provavelmente conhece o sistema de "curtir" do Facebook e outras páginas. 
-As pessoas podem "curtir" postagens de blog, fotos ou outros itens. Queremos criar o texto que deve ser exibido ao lado de tal item.
-Implemente a função que recebe um array contendo os nomes das pessoas que gostam de um item. 
-Ele deve retornar o texto de exibição conforme os exemplos:
-
-[] --> "no one likes this"
-["Peter"]-- > "Peter likes this"
-["Jacob", "Alex"]-- > "Jacob and Alex like this"
-["Max", "John", "Mark"]-- > "Max, John and Mark like this"
-["Alex", "Jacob", "Mark", "Max"]-- > "Alex, Jacob and 2 others like this"
+ * Um pangrama é uma frase que contém todas as letras do alfabeto pelo menos uma vez. 
+ * Por exemplo, a frase "The quick brown fox jumps over the lazy dog." é um pangrama, 
+ * porque usa as letras AZ pelo menos uma vez (as letras maiúsculas e minúsculas são irrelevantes).
+ * Dada uma string, detecte se é ou não um pangrama. Retorna True se for, False se não. Ignore números e pontuação.
+ * 
+ * [Test]
+ * Assert.AreEqual(true, Kata.IsPangram("The quick brown fox jumps over the lazy dog."));
 */
 
-static string Likes(string[] name)
+static bool IsPangram(string str)
 {
-    int len = name.Length;
-    string response = string.Empty;
-
-    switch (len)
-    {
-        case 0:
-             response = "no one likes this";
-            break;
-        case 1:
-             response = $"{name[0]} likes this";
-            break;
-        case 2:
-             response = $"{name[0]} and {name[1]} like this";
-            break;
-        case 3:
-             response = $"{name[0]}, {name[1]} and {name[2]} like this";
-            break;
-        case > 3:
-             response = $"{name[0]}, {name[1]} and {len - 2} others like this";
-            break;
-        default:
-            break;
-    }
-    return response;
+    throw new NotImplementedException();
 }
