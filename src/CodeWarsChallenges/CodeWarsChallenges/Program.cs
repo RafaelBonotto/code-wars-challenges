b06632293a03dc9806adb45de2b20ac7f85d9257
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-02-05 20:46:06 -0300

    checkin

--- src/CodeWarsChallenges/CodeWarsChallenges/Program.cs
+++ src/CodeWarsChallenges/CodeWarsChallenges/Program.cs
@@ -1,4 +1,4 @@
-﻿//CODE WARS CHALLENGE - 'Persistent Bugger.'
+﻿//CODE WARS CHALLENGE - 'Valid Braces.'
 /* Escreva uma função que receba uma string de chaves e determine se a ordem das chaves é válida. 
  * Ele deve retornar truese a string for válida e falsese for inválida.
  * Este Kata é semelhante ao Kata Parênteses Válidos , mas apresenta novos caracteres: colchetes []e chaves {}. 
@@ -20,5 +20,22 @@
 
 static bool validBraces(String braces)
 {
+    bool abriuParenteses;
+    bool abriuChaves;
+    bool abriuColchetes;
+
+    foreach (var caracter in braces) 
+    {
+        if (caracter.Equals('('))
+            abriuParenteses = true;
+
+        if (caracter.Equals('{'))
+            abriuChaves = true;
+
+        if (caracter.Equals('['))
+            abriuColchetes = true;
+    }
+
+
     return false;
 }
