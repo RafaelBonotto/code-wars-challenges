b94d8f6b10faf5cc73516faadbf285fbb1c87f37
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2023-02-05 20:33:41 -0300

    Desafio Valid Braces

--- src/CodeWarsChallenges/CodeWarsChallenges/Program.cs
+++ src/CodeWarsChallenges/CodeWarsChallenges/Program.cs
@@ -1,22 +1,24 @@
 ﻿//CODE WARS CHALLENGE - 'Persistent Bugger.'
-/*
- *Escreva uma função, persistence, que receba um parâmetro positivo nume retorne sua persistência multiplicativa, 
- *que é o número de vezes que você deve multiplicar os dígitos numaté chegar a um único dígito.
- *Por exemplo (Entrada --> Saída) :
- *39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
- *999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
- *4 --> 0 (because 4 is already a one-digit number) 
- *
- *[Test]
- *Assert.AreEqual(3, Persist.Persistence(39));
- *Assert.AreEqual(0, Persist.Persistence(4));
- *Assert.AreEqual(2, Persist.Persistence(25));
- *Assert.AreEqual(4, Persist.Persistence(999));
+/* Escreva uma função que receba uma string de chaves e determine se a ordem das chaves é válida. 
+ * Ele deve retornar truese a string for válida e falsese for inválida.
+ * Este Kata é semelhante ao Kata Parênteses Válidos , mas apresenta novos caracteres: colchetes []e chaves {}. 
+ * Todas as strings de entrada não serão vazias e consistirão apenas em parênteses, colchetes e chaves: ()[]{}.
+ * O que é considerado Válido? 
+ * Uma sequência de chaves é considerada válida se todas as chaves forem combinadas com a chave correta.
+ * 
+ * Exemplos
+ * "(){}[]"   =>  True
+ * "([{}])"   =>  True
+ * "(}"       =>  False
+ * "[(])"     =>  False
+ * "[({})](]" =>  False
+ * 
+ * Assert.AreEqual(true, Brace.validBraces( "()" ));
+ * Assert.AreEqual(false, Brace.validBraces("[(])"));
  */
 
 
-static int Persistence(long n)
+static bool validBraces(String braces)
 {
-    // your code
-    return 0;
+    return false;
 }
